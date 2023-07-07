b15eb4659fa4cd4b99936667283f87250e6f2379
KyalSinOo <okyalsin958gmail.com>
2023-07-07 09:29:21 +0630

    grade calculator

---
+++ in-out.js
@@ -0,0 +1,20 @@
+// console.dir(window);
+// console.dir(document);
+// console.log(console);
+//output
+// console.log("hello");
+// document.write("hello");
+// window.alert("nay kaung lar")
+
+
+//inputyes
+// let a=window.prompt("sar");
+// console.log(a);
+// document.write(a)
+// const yourName=document.getElementById("yourname");
+// const btn=document.getElementById("btn");
+// btn.onclick=()=>{
+//     console.log(yourName.value);
+//     yourName.value=null;
+    
+// };
